ceb310ce07dff3a7b3dc9e0c361653d84e8eb61e
Jinpyo-Ahn <wlsvy3312@o365.hanbat.ac.kr>
2023-11-19 13:28:01 +0900

    [typescipt] 3장 변수와 함수의 타입 정의

---
+++ src/chapter03/ex02.ts
@@ -0,0 +1,22 @@
+// function sayWord(word): string  {
+//   return word;
+// }
+
+// function saywWord(word: string): string {
+//   return word;
+// }
+//
+// saywWord('hi', 'capt');
+
+// function sayMyName(firstName: string, lastName: string): string {
+//   return `my name : ${firstName} ${lastName}`;
+// }
+//
+// console.log(sayMyName('Captain', 'jinpyo'));
+
+function sayMyName(firstName:string, lastName?: string): string {
+  return `my name : ${firstName} ${lastName}`;
+}
+
+sayMyName('Captain');
+
