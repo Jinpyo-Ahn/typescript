028183f7f6af05bd64ea095c64f3edfd3d579c48
Jinpyo-Ahn <wlsvy3312@o365.hanbat.ac.kr>
2023-11-29 21:43:00 +0900

    [typescript] 5장 인터페이스

---
+++ chapter05/src/indexing.ts
@@ -0,0 +1,69 @@
+// const user = {
+//   name: '캡틴',
+//   admin: true
+// };
+// console.log(user['name']);
+//
+// const companies = ['삼성', '네이버', '구글'];
+// console.log(companies[0])
+
+// interface StringArray {
+//   [index: string]: string
+// }
+//
+// const companies: StringArray = ['삼성', '구글', '네이버'];
+
+// interface SalaryMap {
+//   [level: string]: string
+// }
+//
+// const salary: SalaryMap = {
+//   junior: '100',
+// };
+//
+// const money = salary['junior'];
+//
+// console.log(money);
+
+// const salary: SalaryInfo = {
+//   junior: '100원',
+//   mid: '400원',
+//   senior: '700원',
+//   ceo: '0원',
+//   newbie: '50원'
+// };
+//
+// interface SalaryInfo {
+//   [index: string]: string
+// }
+
+// interface User {
+//   id: string,
+//   name: string
+// }
+//
+// const seho: User = {
+//   id: '1',
+//   name: '세호',
+// }
+
+// interface User {
+//   [property: string]: string
+// }
+//
+// const seho: User = {
+//
+// };
+
+// interface User {
+//   [property: string]: string,
+//
+//   id: string,
+//   name: string
+// }
+//
+// const seho: User = {
+//   id: '1',
+//   name: '세호',
+//   address: '판교',
+// }
