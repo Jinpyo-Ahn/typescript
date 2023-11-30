51791071b7ef8a291688210216787bfef6ac0efb
Jinpyo-Ahn <wlsvy3312@o365.hanbat.ac.kr>
2023-11-30 14:15:31 +0900

    6장 연산자를 사용한 타입 정의

---
+++ chapter06/src/union.ts
@@ -0,0 +1,18 @@
+// function logText(text: string | number) {
+//   console.log(text);
+// }
+//
+// logText('hi');
+// logText(100);
+
+// function logText(text: string) {
+//   console.log(text);
+// }
+//
+// function logNumber(text: number) {
+//   console.log(text);
+// }
+
+// function logText(text: string | number) {
+//   console.log(text.tostinrg);
+// }
