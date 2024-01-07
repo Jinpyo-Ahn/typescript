f3bd7522fdce289c85b052e644ea37e1d12b51c2
Jinpyo-Ahn <wlsvy3312@o365.hanbat.ac.kr>
2024-01-07 16:56:42 +0900

    [typescript-seminar] 12장 타입 추론

--- chapter12/src/12-1.ts
+++ chapter12/src/12-1.ts
@@ -9,6 +9,44 @@
 // let a;
 // a = 10;
 
-function sum(a: number, b: number): number {
-  return a + b;
-}
+// function sum(a: number, b: number) {
+//   return a + b;
+// }
+//
+// let result = sum(1, 2);
+
+// function isEqual(a: number, b: number) {
+//   return a === b;
+// }
+
+// function getA(a: number) {
+//   let c = 'hi';
+//   return a + c;
+// }
+
+// interface Dropdown<T> {
+//   title: string,
+//   value: T
+// }
+//
+// let shoppingItem: Dropdown<number> = {
+//   title: '길벗 책',
+//   value
+// }
+
+// interface Dropdown<T> {
+//   title: string,
+//   value: T
+// }
+//
+// interface DetailedDropdown<K> extends Dropdown<K> {
+//   tag: string,
+//   description: string
+// }
+//
+// const shoppingDetailItem: DetailedDropdown<number> = {
+//   title: '길벗 책',
+//   description: '쉽고 유용하다',
+//   tag: '타입스크립트',
+//   value: 1,
+// }
